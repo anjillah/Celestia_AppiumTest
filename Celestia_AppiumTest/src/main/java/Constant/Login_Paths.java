5e276d984a82b61c431766a57380a510bb2503f5
anjillah <alingayjillah@gmail.com>
2024-10-05 01:56:07 +0800

    added constant login appium xpaths

---
+++ Celestia_AppiumTest/src/main/java/Constant/Login_Paths.java
@@ -0,0 +1,11 @@
+package Constant;
+
+public class Login_Paths {
+    public static final String EmailField_APPIUM_xpath = "//androidx.compose.ui.platform.ComposeView/android.view.View/" +
+            "android.view.View/android.view.View/android.widget.EditText[1]";
+
+    public static final String PasswordField_Appium_xpath = "//androidx.compose.ui.platform.ComposeView/android.view.View/" +
+            "android.view.View/android.view.View/android.widget.EditText[2]";
+
+    public static final String LoginButton_Appium_xpath = "//android.widget.Button";
+}
