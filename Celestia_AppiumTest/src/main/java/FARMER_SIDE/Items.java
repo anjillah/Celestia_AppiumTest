4726a5a73cbbd572a2d71e01046ca48d8c89858c
anjillah <alingayjillah@gmail.com>
2024-10-28 18:30:58 +0800

    Items.java

---
+++ Celestia_AppiumTest/src/main/java/FARMER_SIDE/Items.java
@@ -0,0 +1,13 @@
+package FARMER_SIDE;
+
+public class Items {
+    //paths
+    public static final String addProduct = "android:id/farmerAddProductButton";
+    public static final String enterVegName = "android:id/enterVegetableName";
+    public static final String enterQuantity = "android:id/quantityError";
+    public static final String selectStartMonth = "android:id/selectStartMonth";
+    public static final String selectEndMonth = "android:id/seasonEndInput";
+    public static final String dismissButton = "android:id/dismissButton";
+    public static final String confirmButton = "android:id/confirmButton";
+
+}
